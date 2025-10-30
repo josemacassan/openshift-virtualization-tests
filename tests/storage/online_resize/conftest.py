b7505162b0cc99aad7c4abc0a39d45687238384e
Jose Manuel Castano <joscasta@redhat.com>
2025-10-30 15:00:27 +0100

    Apply requested changes from PR review.

--- tests/storage/online_resize/conftest.py
+++ tests/storage/online_resize/conftest.py
@@ -20,7 +20,7 @@ from utilities.virt import VirtualMachineForTests, running_vm
 
 
 @pytest.fixture(scope="module")
-def xfile_skip_if_storage_for_online_resize_does_not_support_snapshots(
+def xfail_skip_if_storage_for_online_resize_does_not_support_snapshots(
     admin_client, storage_class_matrix_online_resize_matrix__module__
 ):
     sc_name = [*storage_class_matrix_online_resize_matrix__module__][0]
