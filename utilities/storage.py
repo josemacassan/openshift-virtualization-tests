aa6d361a825d71a586a32480b82eb9aa7113c236
Jose Manuel Castano <joscasta@redhat.com>
2025-11-26 10:54:36 +0100

    Add function to check rhel vm output.

--- utilities/storage.py
+++ utilities/storage.py
@@ -687,6 +687,10 @@ def write_file(vm, filename, content, stop_vm=True):
     if stop_vm:
         vm.stop(wait=True)
 
+def run_command_on_rhel_vm_and_check_output(vm, command, expected_result):
+    with console.Console(vm=vm) as vm_console:
+        vm_console.sendline(command)
+        vm_console.expect(expected_result, timeout=20)
 
 def write_file_via_ssh(vm: "VirtualMachineForTests", filename: str, content: str) -> None:
     """
