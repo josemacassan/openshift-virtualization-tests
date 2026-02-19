a695774e220b9a4a4e84d52965a1da226871424f
Jose Manuel Castano <joscasta@redhat.com>
2026-02-19 12:07:42 +0100

    Rebert unnedeed changes after cherry-pick

--- tests/install_upgrade_operators/json_patch/utils.py
+++ tests/install_upgrade_operators/json_patch/utils.py
@@ -63,15 +63,8 @@ def get_metrics_value_with_annotation(prometheus, query_string, component_name):
 def filter_metric_by_component(metrics, metric_name, component_name):
     annotation_name = get_annotation_name_for_component(component_name=component_name)
     for metric in metrics:
-        if (
-            metric.get("metric", {}).get("annotation_name") == annotation_name
-            and metric.get("metric", {}).get("__name__") == metric_name
-        ):
-            metric_value = metric.get("value", [])
-            if len(metric_value) > 1:
-                return int(metric_value[1])
-    LOGGER.warning(f"No results found when filtering for {metric_name} and annotation {annotation_name} in {metrics}.")
-    return 0
+        if metric["metric"]["annotation_name"] == annotation_name and metric["metric"]["__name__"] == metric_name:
+            return int(metric["value"][1])
 
 
 def wait_for_metrics_value_update(prometheus, component_name, query_string, previous_value):
